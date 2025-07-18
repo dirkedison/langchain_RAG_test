e1429c4c39e37e36bf35e7f0cffc7feacbeb8662
dirkedison <dirk.edison@gmail.com>
2025-07-18 10:16:49 -0400

    error handling changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 import tempfile
 import requests
 
